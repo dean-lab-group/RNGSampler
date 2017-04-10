16cdada5165df8a0910f31054362fa7d7ecf33b0
azm0043 <andrew.muscha@auburn.edu>
2017-04-10 15:14:03 -0500

    Updated .gitignore and removed build files

--- MDK-ARM/RNGSampler/retarget.d
+++
@@ -1,5 +0,0 @@
-rngsampler\retarget.o: ..\Src\Retarget.c
-rngsampler\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-rngsampler\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\rt_misc.h
-rngsampler\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-rngsampler\retarget.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- MDK-ARM/RNGSampler/startup_stm32f446xx.d
+++
@@ -1,1 +0,0 @@
-rngsampler\startup_stm32f446xx.o: startup_stm32f446xx.s
